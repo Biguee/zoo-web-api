d3785aff2a7cb6096940a9e026a3caf761462d34
Justyna Sałacińska <salacinska.justyna@gmail.com>
2017-10-06 17:31:43 +0200

    security enabled -> false

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -2,5 +2,5 @@ export class Api {
 
   baseUrl = 'http://localhost:8080/api-v1'
   activeAnimal = 'http://localhost:8080/api-v1/animals/active'
-  animalsUrl = 'http://localhost:8080/api-v1/animals'
+  animalsUrl = 'http://localhost:4200/animals-list'
 }
---
+++ src/main/java/pl/com/zoo/common/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package pl.com.zoo.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Locale;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private Mapper mapper;
+
+    @Autowired
+    public GlobalExceptionHandler(Mapper mapper) {
+        this.mapper = mapper;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity onException(Exception ex, Locale locale) {
+        ex.printStackTrace();
+        return new ResponseEntity(mapper.map(ex, locale), INTERNAL_SERVER_ERROR);
+    }
+
+}
